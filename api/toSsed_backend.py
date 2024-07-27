1e3827cda5883ec5c549090c09836f49b9244bd9
Faris aka Animatnetic <faristheknight77@gmail.com>
2024-07-27 20:52:03 +0300

    Test request

--- api/toSsed_backend.py
+++ api/toSsed_backend.py
@@ -5,14 +5,20 @@ from dotenv import load_dotenv
 
 load_dotenv() # Initializing dot environment variables
 api_key = os.getenv("AI71_API_KEY")
-
+model_name = "tiiuae/falcon-180B-chat"
 
 app = Flask(__name__)
 
 
 @app.route("/summarize")
 def home():
-    return jsonify({"status": 200, "message": "This is a test for the GET request"}), 200
+    return client.chat.completions.create(
+        model=model_name, 
+        messages=[
+            {"role": "system", "content": "You are a terms of service summarizer, pretty much, a legal expert to help normal people to understand key points of the ToS, especially those of which breach the user's rights and are most unfair"},
+            {"role": "user", "content": "Hello, what are you?"}
+        ]
+    )
 
 
 @app.errorhandler(404)
