70a2f7296d48a0908d5935e4e11dae6f929babc0
Faris aka Animatnetic <faristheknight77@gmail.com>
2024-07-27 13:00:10 +0300

    Added api key as environment variable for security in a distrobutable manner for futuer collaboraters as well, delete .dist on the .env file and put in your own api key.

--- api/toSsed_backend.py
+++ api/toSsed_backend.py
@@ -1,13 +1,19 @@
+import os
 from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+load_dotenv() # Initializing dot environment variables
+api_key = os.getenv("AI71_API_KEY")
+
 
 app = Flask(__name__)
 
 
 @app.route("/api")
 def home():
-    return "Hello world", 200 # Code 200 means sucess
+    return f"Hello World, {api_key}", 200
 
 
 @app.errorhandler(404)
 def page_not_found(error_message):
-    return jsonify({"status": 404, "message": "Not Found"}), 404 # Error 404 code s
+    return jsonify({"status": 404, "message": "Not Found"}), 404 # Error 404 code
