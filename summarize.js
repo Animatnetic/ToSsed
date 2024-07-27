1ccc4801ba61826c083fbb526c25fc26b7547645
Faris aka Animatnetic <faristheknight77@gmail.com>
2024-07-27 20:44:08 +0300

    Set contnet type for headers.

--- summarize.js
+++ summarize.js
@@ -3,7 +3,10 @@ let summarizeButton = document.getElementById("submitToS");
 
 async function get(url) {
     return fetch(url, {
-        method: "GET"
+        method: "GET", 
+        headers: {
+            "Content-Type": "text/html"
+        }
     })
     .then((response) => response.json())
     .catch((erorrMessage) => console.log("Internal error message from script:", erorrMessage));
