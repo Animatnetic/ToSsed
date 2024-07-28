533fe646d859153ff3c47a42a85af9999df65339
Faris aka Animatnetic <faristheknight77@gmail.com>
2024-07-29 00:56:06 +0300

    Updating the configured maxDuration for responses to be waited for, though this still may not be as suitable as Sockets (maybe)

--- api/toSsed_backend.py
+++ api/toSsed_backend.py
@@ -23,14 +23,14 @@ def summarize_input():
         if text_input == "" or text_input is None:
             return jsonify({"result": None}), 200 # Returning nothing has to not waste tokens/computation on empty inputs.
         else:
-            max_chunk_size = 1500 # Tokens, in this case of which is "characters"
+            max_chunk_size = 1500 # Tokens, in this case, is "characters"
             chunks = [text_input[i: i+max_chunk_size] for i in range(0, len(text_input), max_chunk_size)] # Breaks up the input into chunks of 1500 characters intervals to operate them individually
 
             for chunk_index, chunk in enumerate(chunks):
                 result_chunk = client.chat.completions.create(
                     model=model_name, 
                     messages=[
-                        {"role": "system", "content": "You are a terms of service summarizer, pretty much, a legal expert to help normal people to understand key points of the ToS, especially those of which breach the user's rights and are most unfair. You only return data in JSON format with the value within the key value pair always edited as you see fit according to the inputted prompt. If they did not input a proper ToS, let them know within this JSON strucutre"},
+                        {"role": "system", "content": "You are a terms of service summarizer, pretty much, a legal expert to help normal people to understand key points of the ToS, especially those of which breach the user's rights and are most unfair. You only return data in JSON format with the value within the key value pair always edited as you see fit according to the inputted prompt. If they did not input a proper ToS, let them know within this JSON strucutre. Do not summarize everything, only the more concerning components of the ToS, and only those more concerning ones"},
                         {"role": "user", "content":
                             f"""
                             Give the following summary of the this inputted Terms of Service chunk in the JSON structure below:
@@ -41,10 +41,19 @@ def summarize_input():
                             """
                 }]).choices[0].message.content # Accessing the answer of the request in a non streaming manner as Vercel does not support this for python flask runtime
 
-                result_chunk = result_chunk[1:len(result_chunk)] # Removing that first empty character that is always strangely present on the completions message given by falcon
-                result_chunk = json.loads(result_chunk) # Converting returned stirng into json, but can not return this directly as flask needs to return a specific Response object
+                # result_chunk = result_chunk[1:len(result_chunk)] # Removing that first empty character that is always strangely present on the completions message given by falcon
+                # result_chunk = json.loads(result_chunk) # Converting returned stirng into json, but can not return this directly as flask needs to return a specific Response object
 
-                return jsonify({"summary_title": result_chunk["summary_title"], "summary_meaning": result_chunk["summary_meaning"]}), 200 # Unpacking dictionary into a json structure that is actually returned
+                # return jsonify({"summary_title": result_chunk["summary_title"], "summary_meaning": result_chunk["summary_meaning"]}), 200 # Unpacking dictionary into a json structure that is actually returned
+                    
+            all_results.append(result_chunk)
+            all_results = "".join(all_results)
+
+            all_results = all_results[1:len(all_results)]
+            
+            return jsonify({"summary_title": all_results["summary_title"], "summary_meaning": all_results["summary_meaning"]}), 200
+            # This is returning all in one go. Though, this may even exceed 1 minute response...
+
 
 
 @app.errorhandler(404)
