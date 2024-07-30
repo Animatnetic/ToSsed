46ea5a5bd4e56a815e7f803c84afdb1d05835e00
Faris aka Animatnetic <faristheknight77@gmail.com>
2024-07-30 23:44:45 +0300

    Testing displaying the information to the front end

--- summarize.js
+++ summarize.js
@@ -1,6 +1,30 @@
+// Elements are using bootstrap v5.3
+
 let summarizeButton = document.getElementById("submitToS");
 let inputtedText = document.getElementById("inputTextField");
 let warningModal = document.getElementById("warningModal");
+
+let summarizeSection = document.querySelector("section");
+
+let accordianDivContainer = document.createElement("div");
+accordianDivContainer.setAttribute("class", "accordian");
+accordianDivContainer.setAttribute("id", "outputAccordian");
+
+let gradeElement = document.createElement("div");
+gradeElement.setAttribute("class", "alert alert-light");
+gradeElement.setAttribute("role", "alert");
+gradeElement.innerHTML = 
+`
+<div class="fw-bold">
+    Grade: <span class="badge"></span>
+</div>
+`;// class="text-bg-[variable colour depending on grade]"
+
+let alertElement = document.createElement("div");
+alertElement.setAttribute("class", "alert alert-warning d-flex");
+alertElement.setAttribute("style", "justify-content: center;");
+alertElement.setAttribute("role", "alert");
+alertElement.innerHTML = " Keep in mind, this is an artifical intelligent model so summaries or grading may be incorrect.";
 
 
 async function get(url) {
@@ -24,7 +48,10 @@ async function post(url, payload) {
         }
     })
     .then((response) => response.json())
-    .catch((erorrMessage) => console.log("Internal error message from script:", erorrMessage));
+    .catch((erorrMessage) => {
+        console.log("Internal error message from script:", erorrMessage);
+        alert("An unexpected error occured!");
+    });
 };
 
 
@@ -40,16 +67,38 @@ async function fetchSummary() {
                 <span class="visually-hidden">Loading...</span>
             </div>
         </div>
-    `;
+    `; // HTML for the loading screen
     summarizeButton.appendChild(loadingElement);
 
-    // let result = await get(`https://tossed-away.vercel.app/summarize?input=${inputtedText.value}`);
     let result = await post("https://tossed-away.vercel.app/summarize", inputtedText.value);
     
     // Allow the button to be pressed again, and remove the loading after the data has been retreived 
     summarizeButton.removeChild(loadingElement);
     summarizeButton.disabled = false;
-    console.log(result);
+
+    for (let summaryPointIndex = 0; summaryPointIndex <= result["all_summaries"].length; summaryPointIndex ++) {
+        // String template for creating accordian elements in a programmatic manner
+        let accordianIdentifier = `collapse${summaryPointIndex}` // Uniquely identify each accordian element
+
+        let accordianElementTemplate = 
+        `
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="${accordianIdentifier}" aria-controls="${accordianIdentifier}">
+            ${result["all_summaries"][summaryPointIndex]["summary_title"]}
+            </button>
+            </h2>
+            <div id="${accordianIdentifier}" class="accordion-collapse collapse">
+            <div class="accordion-body">
+            ${result["all_summaries"][summaryPointIndex]["summary_meaning"]}
+            </div>
+            </div>
+        </div>
+        `;
+// Do note, I set the data-bs-controls and aria-controls programmatically 
+
+        accordianDivContainer.appendChild(accordianElementTemplate);
+    };
 };
 
 
@@ -58,8 +107,6 @@ summarizeButton.onclick = fetchSummary;
 inputtedText.addEventListener("input", () => {
     console.log("Input logged");
 
-    // data-bs-="modal" data-bs-target="#warningModal"
-
     if (inputtedText.value == "") {
         summarizeButton.setAttribute("data-bs-toggle", "modal");
         summarizeButton.setAttribute("data-bs-target", "#warningModal");
@@ -67,4 +114,5 @@ inputtedText.addEventListener("input", () => {
         summarizeButton.removeAttribute("data-bs-toggle", "modal");
         summarizeButton.removeAttribute("data-bs-target", "#warningModal");
     };
+    // Warning the user for if they summarize a blank input as to not waste AI computation
 });
