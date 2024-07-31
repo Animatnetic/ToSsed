98f4d97f4e66e8f6460b7cd906d4be7dbd3e00a2
Faris aka Animatnetic <faristheknight77@gmail.com>
2024-07-31 15:17:36 +0300

    turned function into a courtine (async def instead of just def)

--- api/toSsed_backend.py
+++ api/toSsed_backend.py
@@ -15,25 +15,29 @@ model_name = "tiiuae/falcon-180b-chat"
 client = AI71(API_KEY)
 
 
+async def promptFalcon(chunk):
+    client.chat.completions.create(
+    model=model_name, 
+    messages=[
+    {"role": "system", "content": "You are a terms of service summarizer, pretty much, a legal expert to help normal people to understand key points of the ToS, especially those of which breach the user's rights and are most unfair. You only return data in JSON format with the value within the key value pair always edited as you see fit according to the inputted prompt. If they did not input a proper ToS, let them know within this JSON strucutre. Do not summarize everything, only the more concerning components of the ToS, and only those more concerning ones"},
+    {"role": "user", "content":
+        f"""
+        Give the following summary of the this inputted Terms of Service in the JSON structure below:
+
+        {{"summary_title": "A brief summary of this part of the terms of service highlighting only more unfair/concerning part of the ToS", "summary_meaning": "A more in-depth elaboration of the summary and what it means, as well as the specific quotations sourced from the Terms of Service, ensure to incase in quotation marks to make those quotes explicit"}}
+
+        prompt: {chunk}
+        """}]
+    )
+
+    # Turning the chat.completions.create() function into a coroutine
+
+
 def get_tasks(chunks): # Defining event loop of tasks to be ran asynchronously, side by side.
     tasks = []
 
-    for chunkIndex, chunk in enumerate(chunks):
-        task = asyncio.create_task(client.chat.completions.create(
-            model=model_name, 
-            messages=[
-            {"role": "system", "content": "You are a terms of service summarizer, pretty much, a legal expert to help normal people to understand key points of the ToS, especially those of which breach the user's rights and are most unfair. You only return data in JSON format with the value within the key value pair always edited as you see fit according to the inputted prompt. If they did not input a proper ToS, let them know within this JSON strucutre. Do not summarize everything, only the more concerning components of the ToS, and only those more concerning ones"},
-            {"role": "user", "content":
-                f"""
-                Give the following summary of the this inputted Terms of Service in the JSON structure below:
-
-                {{"summary_title": "A brief summary of this part of the terms of service highlighting only more unfair/concerning part of the ToS", "summary_meaning": "A more in-depth elaboration of the summary and what it means, as well as the specific quotations sourced from the Terms of Service, ensure to incase in quotation marks to make those quotes explicit"}}
-
-                prompt: {chunk}
-                """}]
-        )) # Encasing the call in a couroutine
-
-        tasks.append(task)
+    for chunkIndex, chunk in enumerate(chunks): # Encasing the call in a couroutine
+        tasks.append(asyncio.create_task(promptFalcon(chunk)))
 
     return tasks
 
